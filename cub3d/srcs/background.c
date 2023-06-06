419e8173c57107db1e113ed2376e564b27d4594b
dpavon-g <daniti333@gmail.com>
2023-06-06 20:01:16 +0200

    cub3d update

--- cub3d/srcs/background.c
+++ cub3d/srcs/background.c
@@ -1,30 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_matrix.c                                     :+:      :+:    :+:   */
+/*   background.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/15 20:09:45 by ldurante          #+#    #+#             */
-/*   Updated: 2022/02/15 13:30:19 by ldurante         ###   ########.fr       */
+/*   Created: 2022/02/24 17:17:17 by ldurante          #+#    #+#             */
+/*   Updated: 2022/03/14 13:06:56 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../includes/cub3d.h"
 
-void	print_matrix(char **matrix)
+void	draw_background(t_img bg, int ceiling, int floor)
 {
-	int	i;
+	int	x;
+	int	y;
+	int	texture;
 
-	i = 0;
-	if (!matrix)
-		ft_printf("(null)\n");
-	else
+	y = 0;
+	texture = ceiling;
+	while (y < WIN_HEIGHT)
 	{
-		while (matrix[i])
+		if (y == WIN_HALF)
+			texture = floor;
+		x = 0;
+		while (x < WIN_WIDTH)
 		{
-			ft_printf("|%s|\n", matrix[i]);
-			i++;
+			my_mlx_pixel_put(&bg, x, y, texture);
+			x++;
 		}
+		y++;
 	}
 }
--- cub3d/srcs/main.c
+++ cub3d/srcs/main.c
@@ -6,161 +6,64 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 00:02:49 by ldurante          #+#    #+#             */
-/*   Updated: 2022/02/11 01:35:11 by ldurante         ###   ########.fr       */
+/*   Updated: 2022/03/16 12:01:58 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-char	**ft_realloc(char **info, char **line)
+void	free_data(t_data *data)
 {
-	int		size;
-	char	**aux;
-	int		i;
-
-	i = 0;
-	aux = NULL;
-	if (!(info))
-	{
-		aux = malloc(sizeof(char *) * 2);
-		aux[0] = *line;
-		aux[1] = NULL;
-		return (aux);
-	}
-	size = matrix_len(info);
-	aux = malloc(sizeof(char *) * (size + 2));
-	while (i < size)
-	{
-		aux[i] = ft_strdup(info[i]);
-		i++;
-	}	
-	aux[size] = *line;
-	aux[size + 1] = NULL;
-	return (aux);
+	if (data->no)
+		free(data->no);
+	if (data->we)
+		free(data->we);
+	if (data->ea)
+		free(data->ea);
+	if (data->so)
+		free(data->so);
+	if (data->cei)
+		free(data->cei);
+	if (data->fl)
+		free(data->fl);
+	if (data->map)
+		free_matrix(data->map);
 }
 
-char	**get_info(char **argv)
+int	close_game(t_game *g)
 {
-	int		fd;
-	char	**info;
-	char	*line;
-	char	**aux;
-	int		ret;
-
-	line = NULL;
-	info = NULL;
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-		printf("Error\n%s\n", ERR_FILE);
-	ret = get_next_line(fd, &line);
-	while (ret >= 0)
-	{
-		aux = ft_realloc(info, &line);
-		free_matrix(info);
-		info = matrix_dup(aux);
-		free_matrix(aux);
-		if (ret == 0)
-			break ;
-		ret = get_next_line(fd, &line);
-	}
-	close(fd);
-	return (info);
-}
-
-int	parse_data_aux(t_data *data, char ***aux)
-{
-	if ((*aux)[0])
-	{
-		if (!ft_strncmp((*aux)[0], "NO", 3) && !data->no)
-			data->no = matrix_dup((*aux));
-		else if (!ft_strncmp((*aux)[0], "SO", 3) && !data->so)
-			data->so = matrix_dup((*aux));
-		else if (!ft_strncmp((*aux)[0], "WE", 3) && !data->we)
-			data->we = matrix_dup((*aux));
-		else if (!ft_strncmp((*aux)[0], "EA", 3) && !data->ea)
-			data->ea = matrix_dup((*aux));
-		else if (!ft_strncmp((*aux)[0], "F", 2) && !data->fl)
-			data->fl = matrix_dup((*aux));
-		else if (!ft_strncmp((*aux)[0], "C", 2) && !data->cei)
-			data->cei = matrix_dup((*aux));
-		else
-		{
-			printf("Error\n%s\n", ERR_ID);
-			free_matrix ((*aux));
-			return (1);
-		}
-		free_matrix((*aux));
-		(*aux) = NULL;
-	}
+	mlx_destroy_image(g->ptr, g->bg.img);
+	mlx_destroy_image(g->ptr, g->mini_map.img);
+	mlx_destroy_image(g->ptr, g->tex.no.img);
+	mlx_destroy_image(g->ptr, g->tex.so.img);
+	mlx_destroy_image(g->ptr, g->tex.ea.img);
+	mlx_destroy_image(g->ptr, g->tex.we.img);
+	mlx_destroy_window(g->ptr, g->win);
+	free_matrix(g->map);
+	exit (0);
 	return (0);
 }
 
-void	get_map(char **info, t_data *data)
+int	check_xpm_files(t_data *data)
 {
-	int		i;
-	char	**aux;
+	int	fd;
 
-	i = 0;
-	while (info[i])
-	{
-		if (info[i][0])
-		{
-			aux = ft_split(info[i], ' ');
-			if (aux[0])
-			{
-				free_matrix(aux);
-				break ;
-			}
-			free_matrix(aux);
-		}
-		i++;
-	}
-	data->map = info + i;
-}
-
-void	parse_data(char **info, t_data *data)
-{
-	int		i;
-	int		err;
-	char	**aux;
-
-	i = -1;
-	err = 0;
-	aux = NULL;
-	while (info[++i])
-	{
-		if (data->no && data->so && data->we && data->ea && data->fl
-			&& data->cei)
-			break ;
-		if (info[i][0])
-		{
-			aux = ft_split(info[i], ' ');
-			if (parse_data_aux(data, &aux))
-			{
-				err = 1;
-				break ;
-			}
-			free_matrix(aux);
-		}
-	}
-	if (!err)
-		get_map(info + i, data);
-}
-
-int	check_file_extension(char *argv)
-{
-	int		len;
-	char	*aux;
-
-	len = ft_strlen(argv);
-	aux = ft_substr(argv, len - 4, len);
-	if (ft_strcmp(aux, ".cub"))
-	{
-		printf("Error\n%s\n", ERR_EXT);
-		free(aux);
-		exit (0);
-	}
-	free(aux);
+	fd = open(data->no, O_RDONLY);
+	if (fd < 0)
+		return (1);
+	close(fd);
+	fd = open(data->so, O_RDONLY);
+	if (fd < 0)
+		return (1);
+	close(fd);
+	fd = open(data->ea, O_RDONLY);
+	if (fd < 0)
+		return (1);
+	close(fd);
+	fd = open(data->we, O_RDONLY);
+	if (fd < 0)
+		return (1);
+	close(fd);
 	return (0);
 }
 
@@ -168,19 +71,25 @@ int	main(int argc, char **argv)
 {
 	char	**info;
 	t_data	data;
+	t_game	g;
 
+	ft_bzero(&g, sizeof(t_game));
 	ft_bzero(&data, sizeof(t_data));
-	if (argc == 2 && !check_file_extension(argv[1]))
+	if (argc == 2 && !check_file_extension(argv[1], ".cub", ERR_EXT_FILE))
 	{
 		info = get_info(argv);
 		if (!info)
 			return (0);
-		parse_data(info, &data);
-		// print_matrix(data.map);
-		free_matrix(info);
+		if (!parse_data(info, &data) && !check_data(&data, &g))
+		{
+			if (check_xpm_files(&data))
+				printf("Error\n%s\n", ERR_XPM);
+			else
+				init_cube(&data, &g);
+		}
+		free_data(&data);
 	}
-	else
+	else if (argc != 2)
 		printf("Error\n%s\n", ERR_ARG);
-	// system("leaks -q cub3d");
 	return (0);
 }
