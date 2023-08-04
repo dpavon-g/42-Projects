5f22e3e791d839c8d30428421dc2fea77bb016c8
DanielPavonGomez <daniel.gomezpavon.practicas@telefonica.com>
2023-08-04 21:14:37 +0200

    cpp06 ex00 made hehe

--- cpp_modules/cpp06/ex00/convert.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef CONVERT_HPP
-# define CONVERT_HPP
-
-# include <iostream>
-# include <string>
-	int	argument_type(const std::string &arg);
-#endif
--- cpp_modules/cpp06/ex00/main.cpp
+++ cpp_modules/cpp06/ex00/main.cpp
@@ -1,9 +1,10 @@
-#include "convert.hpp"
+#include "Convert.hpp"
 
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
 		std::string str = argv[argc-1];
-		argument_type(str);
+		Convert conversor;
+		conversor.startConvert(str);
 		return (0);
 	}
 	else {
