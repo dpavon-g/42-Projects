1c0a2d5f697a236bd495535c98428e1fd61b403c
DanielPavonGomez <daniel.gomezpavon.practicas@telefonica.com>
2023-10-21 16:44:40 +0200

    cpp08 made

--- cpp_modules/cpp08/ex01/Span.cpp
+++ cpp_modules/cpp08/ex01/Span.cpp
@@ -1,11 +1,25 @@
 #include "Span.hpp"
 
+
 Span::Span(unsigned int n): _maxIntegers(n) {
     std::cout << "Number <Span> constructor called" << std::endl;
 }
 
 Span::Span(): _maxIntegers(0) {
     std::cout << "Default empty <Span> constructor called" << std::endl;
+}
+
+Span::Span(Span const &copy) {
+    *this = copy;
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Span &Span::operator=(Span const &copy) {
+    if (this != &copy) {
+        this->_maxIntegers = copy._maxIntegers;
+        this->_vec = copy._vec;
+    }
+    return *this;
 }
 
 void    Span::addNumber(int n) {
@@ -17,9 +31,43 @@ void    Span::addNumber(int n) {
     return;
 }
 
-int Span::shortestSpan() {
-    for (std::size_t i = 0; i < this->vec.size(); i++) {
-        
+int		Span::shortestSpan() {
+	int diff;
+	int span = INT_MAX;
+	if (this->_vec.size() <= 1)
+		throw std::runtime_error("Error. Cant calculate span");
+	std::vector<int> tmp(_vec.size());
+	std::copy(_vec.begin(), _vec.end(), tmp.begin());
+	std::sort(tmp.begin(), tmp.end());
+    int i = 0;
+    while (i < (int)tmp.size() - 1) {
+        diff = tmp[i] - tmp[i - 1];
+		if (diff && diff < span)
+			span = diff;
+        i++;
+    }
+	if (span == INT_MAX)
+		throw std::runtime_error("Error. No span found");
+	return span;
+}
+
+int		Span::longestSpan() {
+	if (this->_vec.size() <= 1)
+		throw std::runtime_error("Error. Cant calculate span");
+	int span =	*std::max_element(_vec.begin(), _vec.end())
+		-	*std::min_element(_vec.begin(), _vec.end());
+	if (!span)
+		throw std::runtime_error("Error. No span found");
+	return span;
+}
+
+void Span::getNumbers() {
+    int i;
+
+    i = 0;
+    while (i < (int)_vec.size()) {
+        std::cout << "Number: " << _vec[i] << std::endl;
+        i++;
     }
 }
 
